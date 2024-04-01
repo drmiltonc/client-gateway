88c726373b99aee8fe965529e9b5807e051b2d08
drmiltonc <92145221+drmiltonc@users.noreply.github.com>
2024-03-31 23:31:35 -0500

    Get products

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -1,14 +1,14 @@
 import { Module } from '@nestjs/common';
 import { ProductsController } from './products.controller';
 import { ClientsModule, Transport } from '@nestjs/microservices';
-import { PRODUCTO_SERVICE, envs } from 'src/config';
+import { PRODUCT_SERVICE, envs } from 'src/config';
 
 @Module({
   controllers: [ProductsController],
   providers: [],
   imports: [
     ClientsModule.register([
-      { name: PRODUCTO_SERVICE,
+      { name: PRODUCT_SERVICE,
         transport: Transport.TCP,
         options: {
           host: envs.productHost,
